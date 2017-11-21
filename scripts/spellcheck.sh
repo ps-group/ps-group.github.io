b1e64a6cccfddd7a4ff04387013c8608eef2d719
Sergey Shambir <sshambir@yandex.ru>
2017-11-21 12:17:10 +0300

    spellcheck.sh теперь создаёт HTML-отчёт (средствами yaspeller)

--- scripts/spellcheck.sh
+++ scripts/spellcheck.sh
@@ -3,7 +3,7 @@
 
 DIRECTORY=$1
 if [ -d "$DIRECTORY" ]; then
-  yaspeller --ignore-latin --ignore-digits -e ".md" "$DIRECTORY"
+  yaspeller --ignore-latin --ignore-digits --report html -e ".md" "$DIRECTORY"
 else
   echo ERROR: "$DIRECTORY" does not exist
 fi
